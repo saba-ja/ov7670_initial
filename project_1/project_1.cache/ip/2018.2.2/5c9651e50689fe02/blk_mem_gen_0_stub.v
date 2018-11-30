c9b2270ebf35c5d01c09f2d8d7438639e3af4fa8
Saba Janamian <saba.janamian.663@my.csun.edu>
2018-11-30 15:22:12 -0800

    add: initial commit

---
+++ project_1/project_1.cache/ip/2018.2.2/5c9651e50689fe02/blk_mem_gen_0_stub.v
@@ -0,0 +1,30 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.2.2 (lin64) Build 2348494 Mon Oct  1 18:25:39 MDT 2018
+// Date        : Fri Nov 30 14:00:56 2018
+// Host        : saba-VirtualBox running 64-bit Ubuntu 18.04.1 LTS
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ blk_mem_gen_0_stub.v
+// Design      : blk_mem_gen_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_1,Vivado 2018.2.2" *)
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(clka, wea, addra, dina, douta, clkb, web, addrb, dinb, 
+  doutb)
+/* synthesis syn_black_box black_box_pad_pin="clka,wea[0:0],addra[18:0],dina[11:0],douta[11:0],clkb,web[0:0],addrb[18:0],dinb[11:0],doutb[11:0]" */;
+  input clka;
+  input [0:0]wea;
+  input [18:0]addra;
+  input [11:0]dina;
+  output [11:0]douta;
+  input clkb;
+  input [0:0]web;
+  input [18:0]addrb;
+  input [11:0]dinb;
+  output [11:0]doutb;
+endmodule
